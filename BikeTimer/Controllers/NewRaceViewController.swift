ee0b0a13d78c06dba58bd58774f6527ae10601c9
drewburns <drewburnsbab@gmail.com>
2019-05-20 18:38:28 -0400

    first commit

---
+++ BikeTimer/Controllers/NewRaceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewRaceViewController.swift
+//  BikeTimer
+//
+//  Created by Andrew Burns on 3/29/19.
+//  Copyright © 2019 Andrew Burns. All rights reserved.
+//
+
+import UIKit
+
+class NewRaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BikeTimer/TimeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TimeTableViewCell.swift
+//  BikeTimer
+//
+//  Created by Andrew Burns on 10/18/18.
+//  Copyright © 2018 Andrew Burns. All rights reserved.
+//
+
+import UIKit
+
+class TimeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
